bc51986996a03b8d774eb60e549fccf8513c5f64
Joe Shepley <jshepley14@gmail.com>
2017-01-24 21:35:28 -0500

    hello World code

---
+++ example.java
@@ -0,0 +1,7 @@
+public class example {
+	public static void main(String[] args){
+	//this line is a comment, not code
+	System.out.println("Hello, World!");
+	System.out.println("here is another line to print");
+	}
+}
